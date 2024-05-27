95f59577ab3a7bb520e7fe692080b1776e820956
laboratorio <laboratorio056@uteq.edu.ec>
2024-05-27 12:14:37 -0500

    Adding code of Login Practice

--- app/src/main/java/com/example/smupractica27_05_2024/MainActivity.java
+++ app/src/main/java/com/example/smupractica27_05_2024/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.smupractica27_05_2024;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +25,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void clickBotonIngresar(View view){
+        TextInputLayout tilUsuario = findViewById(R.id.tilUsuario);
+        TextInputEditText txtCedula = findViewById(R.id.txtCedula);
+        String cedula = txtCedula.getText().toString();
+        if (cedula.equals("") || cedula.length()!=10{
+            tilUsuario.setError("Ingrese una cédula");
+        }else
+            tilUsuario.setError("");
+    }
+
+    public void clickBotonCancelar(View view){
+
+        TextInputLayout tilUsuario = findViewById(R.id.tilUsuario);
+        TextInputEditText txtClave = findViewById(R.id.txtClave);
+        String cedula = txtClave.getText().toString();
+        if (cedula.equals("")){
+            tilUsuario.setError("Ingrese una clave");
+        }else
+            tilUsuario.setError("");
+        }
     }
 }
---
+++ app/src/main/java/com/example/smupractica27_05_2024/View.java
@@ -0,0 +1,4 @@
+package com.example.smupractica27_05_2024;
+
+public class View {
+}
